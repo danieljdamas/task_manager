e25b77632353ce774bb8db521564bf39fd664e51
Raghu Betina <raghu@firstdraft.com>
2019-05-25 19:08:07 +0000

    Direct association between tasks and task_tags

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
   # Direct associations
 
+  has_many   :task_tags,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/task_tag.rb
+++ app/models/task_tag.rb
@@ -1,6 +1,8 @@
 class TaskTag < ApplicationRecord
   # Direct associations
 
+  belongs_to :task
+
   # Indirect associations
 
   # Validations
